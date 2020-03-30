6667070b1d0fd22e72d60bfdffc03961ed4df0f4
Arnaud_Oltra <arnaudoltra@hotmail.fr>
2020-03-30 09:00:54 +0200

    Contrainte & mise en forme formulaire

--- contact/src/Form/PersonType.php
+++ contact/src/Form/PersonType.php
@@ -20,6 +20,7 @@ class PersonType extends AbstractType
     {
         $builder
             ->add('gender', ChoiceType::class, [
+                'label' => 'Civilité :',
                 'choices' => [
                     'Monsieur' => 'mr',
                     'Madame' => 'me',
@@ -29,28 +30,35 @@ class PersonType extends AbstractType
                 'multiple' => false,
             ])
             ->add('name', TextType::class, [
+                'label' =>'Prénom :',
                 'constraints' => [
                     new NotBlank(),
                 ]
             ])
             ->add('familyName', TextType::class, [
+                'label' => 'Nom :',
                 'constraints' => [
                     new NotBlank(),
                 ]
             ])
             ->add('address', TextType::class, [
+                'label' => 'Adresse postal :',
                 'required' => false,
             ])
             ->add('email', EmailType::class, [
+                'label' => 'Adresse email :',
                 'required' => false,
             ])
             ->add('phone', TelType::class, [
+                'label' => 'Numéro de téléphone :',
                 'required' => false,
             ])
             ->add('affiliation', TextType::class, [
+                'label' => 'Entreprise :',
                 'required' => false,
             ])
             ->add('tags', EntityType::class, [
+                'label' => 'Fait parti de :',
                 'class' => Tag::class,
                 'expanded' => true,
                 'multiple' => true,
